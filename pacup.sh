f49cd74f3d2e91bf502b43a73025f8673a62040d
Syuugo <pub@s1204.me>
2022-10-30 06:43:09 +0900

    Unification (#1)

--- pacup.sh
+++ pacup.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-
-# ヘルプ
 function usage() {
   cat <<EOM
 使用方法: $(basename $0) [オプション]...
@@ -11,7 +9,6 @@ function usage() {
 EOM
 }
 
-# オプションの読み取り
 while (($#>0)); do
   case $1 in
     a|-a|--apt)
@@ -29,14 +26,8 @@ while (($#>0)); do
       exit 1
     ;;
   esac
-  # $1が無くなるまで繰り返す
   shift
 done
-
-function PACUP_SYS() {
-  # install_pacup.sh から書込
-  : #SYS
-}
 
 function PACUP_APT() {
   echo -e "\napt-get update を実行します"
@@ -47,14 +38,22 @@ function PACUP_APT() {
   sudo apt-get autoremove$PACUP_YES
 }
 
-# APTでの更新のみ実行
+function PACUP_SYS() {
+  if [ $(command -v snap) ]; then
+    echo -e "\nsudo snap refresh を実行します"
+    sudo snap refresh
+  fi
+  if [ $(command -v flatpak) ]; then
+    echo -e "\nsudo flatpak update$PACUP_YES を実行します"
+    sudo flatpak update$PACUP_YES
+  fi
+}
+
 if [ "$PACUP_MOD" == "apt" ]; then
   sudo echo -n
-  # Root要求が失敗した際にリトライ
   if [ $? != 0 ]; then
     echo -e "\nAPTコマンドの実行はRoot(管理者)権限を要求します｡\nもう一度お試しください｡\n"
     sudo echo -n
-    # リトライも失敗した際にエラーで終了
     if [ $? != 0 ]; then
       echo -e "\n権限の昇格に失敗したため､ 実行を終了しました｡"
       exit 1
@@ -74,11 +73,11 @@ if [ $? != 0 ]; then
   fi
 fi
 
-# APTでの更新
 PACUP_APT
-# Flatpakコマンドが存在する場合に install_pacup.sh より書込
-#FPK
-# RootユーザーでのSnapとFlatpakの更新
+if [ -f /usr/bin/flatpak ]; then
+  echo -e "\nflatpak update$PACUP_YES を実行します"
+  flatpak update$PACUP_YES
+fi
 PACUP_SYS
 echo -e "\n完了しました！"
 exit 0
